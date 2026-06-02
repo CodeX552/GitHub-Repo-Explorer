88d04201a326eab3a1ef0ca379e7906979d609b6
codeX552 <prateekgiri08@gmail.com>
2026-06-02 15:27:19 +0530

    Set API Token

--- server/src/utils/axiosInstance.js
+++ server/src/utils/axiosInstance.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 
 const axiosInstance = axios.create({
   baseURL: process.env.GITHUB_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+  },
 });
 
 module.exports = axiosInstance;
